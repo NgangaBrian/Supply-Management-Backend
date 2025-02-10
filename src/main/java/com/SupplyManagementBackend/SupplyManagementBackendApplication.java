d62e51daee1a58d8878a27ec3e6646db173da6f3
NgangaBrian <ngangabrian724@gmail.com>
2025-02-10 22:29:59 +0300

    Progress Commit

---
+++ src/main/java/com/SupplyManagementBackend/SupplyManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SupplyManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SupplyManagementBackend/SupplyManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SupplyManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
